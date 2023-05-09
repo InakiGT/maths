ed69364827d61bc2609e7455b3b0fe736eeb9938
Iñaki García <yarimfuego@gmail.com>
2023-05-09 17:54:19 -0600

    ApiKey guard added and put it into AppController

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProblemsModule } from './problems/problems.module';
+import { AuthModule } from './auth/auth.module';
+import { environments } from './environments';
+import config from './config';
 
 @Module({
-  imports: [ProblemsModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: environments[process.env.NODE_ENV],
+      load: [config],
+      isGlobal: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.string().required(),
+      }),
+    }),
+    ProblemsModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/auth/guards/api-key.guard.ts
@@ -0,0 +1,33 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { ConfigType } from '@nestjs/config';
+import { Request } from 'express';
+
+import config from '../../config';
+
+@Injectable()
+export class ApiKeyGuard implements CanActivate {
+  constructor(
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
+  ) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest<Request>();
+    const authHeader = request.header('Auth');
+    const isAuth = authHeader === this.configService.apiKey;
+
+    if (!isAuth) {
+      throw new UnauthorizedException('Not allowed');
+    }
+
+    return isAuth;
+  }
+}
